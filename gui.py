5e90b27e8c32106664e9c616ee61d95a9feda508
Anubhav Dinkar <anubhavdinkar05@gmail.com>
2019-10-15 05:33:57 +0530

    gui

--- gui.py
+++ gui.py
@@ -7,10 +7,12 @@ def pressed(r, c, size):
 
 def main(size):
     root = Tk()
+    buttons = [Button() for _ in range(size**2)]
     for i in range(size):
         for j in range(size):
-            button = Button(command=lambda a=i, b=j, c=size: pressed(a, b, c))
-            button.grid(row=i, column=j)
+            buttons[i][j] = Button(command=lambda a=i,b=j, c=size: pressed(a, b, c), text="")
+            buttons[i][j].config(text="1")
+            buttons[i][j].grid(row=i, column=j)
     window_width = root.winfo_reqwidth()
     window_height = root.winfo_reqheight()
     position_right = int(root.winfo_screenwidth() / 2 - window_width / 2)
