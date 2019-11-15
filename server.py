a0dbcb139f94b9566254ebecd104696e799155e5
Anubhav Dinkar <anubhavdinkar05@gmail.com>
2019-11-15 20:44:41 +0530

    comments

--- server.py
+++ server.py
@@ -5,22 +5,30 @@ from common import *
 
 
 def main():
+    """
+    AF_INET is to connect to IPv4 type of addresses (AF_INET6 can be used for IPv6)
+    SOCK_STREAM specifies TCP
+    https://en.wikipedia.org/wiki/Internet_Protocol
+    https://en.wikipedia.org/wiki/Transmission_Control_Protocol
+    """
     marker = 1
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     port = 3135
     socket.SO_REUSEPORT = True
-    s.bind(('0.0.0.0', port))
+    s.bind(('0.0.0.0', port))  # binds to a free address
 
     n = int(input("Enter the grid size\n"))
     grid = create_grid(n)
     print('Socket bound to port ', port)
-    s.listen(3)
+    s.listen(3)  # Queue up to arg number of requests
 
     print_grid(grid)
-    c, addr = s.accept()
+    c, addr = s.accept()  # Establish the connection
     print('Got connection from ', addr)
 
-    c.send(str(n).encode())
+    c.send(str(n).encode())  # sends byte like object of the data to the connected system
+
     while True:
         system('clear')
         print_grid(grid)
@@ -43,11 +51,11 @@ def main():
                 print(e[1])  # win message
                 c.send(e[1].encode())
                 break
-            # otherwise, game is not over
-            else:
+
+            else:  # otherwise, game is not over
                 c.send(str(pos).encode())
 
-        a = c.recv(1024).decode()
+        a = c.recv(1024).decode()  # the sent message can have a max length of 1024 bytes
 
         try:
             played = int(a)
